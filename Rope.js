9cf404c7791bbd09ad107918b9584316e8418488
yash-dotcom-js <83535992+yash-dotcom-js@users.noreply.github.com>
2022-01-03 11:01:35 +0000

    Add files via upload
    This is right. Sincerely made by Yash

---
+++ Rope.js
@@ -0,0 +1,28 @@
+class Rope {
+  constructor(bodyA, pointB) {
+    var options = {
+      bodyA: bodyA,
+      pointB: pointB,
+      stiffness: 1.2,
+      length: 450,
+    };
+
+    this.pointB = pointB;
+    this.rope = Constraint.create(options);
+    World.add(world, this.rope);
+  }
+    
+  display() {
+    if (this.rope.bodyA) {
+      var pointA = this.rope.bodyA.position;
+      var pointB = this.pointB;
+      push();
+
+      //set the value of stroke to hide the rope
+      stroke(48, 22, 8);
+      strokeWeight(4);
+
+      pop();
+    }
+  }
+}
